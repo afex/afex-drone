496346eea5450a9bda3e44ce77ecafdd1f7c42ce
Keith Thornhill <kthornhill@twitch.tv>
2014-02-11 20:46:34 -0800

    download latest drone deb

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,3 +1,11 @@
 class drone::install {
-	
+	exec { 'retrieve_drone':
+		command => "/usr/bin/wget http://downloads.drone.io/latest/drone.deb -O /tmp/drone.deb",
+		creates => "/tmp/drone.deb",
+	}
+
+	file { '/tmp/drone.deb':
+		mode => 0644,
+		require => Exec["retrieve_drone"],
+	}
 }
